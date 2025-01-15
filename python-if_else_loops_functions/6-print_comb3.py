eb871df3a8a7c5c9ee0a2d9a092215315257374c
seb25000 <seb25000@gmail.com>
2025-01-15 14:43:00 +0100

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{:02d}{:02d}".format(i, j))
+            print("{}{}".format(i, j))
         else:
-            print("{:02d}{:02d}, ".format(i, j), end="")
+            print("{}{}, ".format(i, j), end="")
