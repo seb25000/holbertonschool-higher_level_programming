05fa25789a477e1a1aab3224bfe07253adb9927d
seb25000 <seb25000@gmail.com>
2025-01-15 20:03:00 +0100

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1
+    
+    a = 10
+    b = 5
+
+    add_result = calculator_1.add(a, b)
+    sub_result = calculator_1.sub(a, b)
+    mul_result = calculator_1.mul(a, b)
+    div_result = calculator_1.div(a, b)
+
+
+    print("a + b = {}".format(add_result))
+    print("a - b = {}".format(sub_result))
+    print("a * b = {}".format(mul_result))
+    print("a / b = {}".format(div_result))
