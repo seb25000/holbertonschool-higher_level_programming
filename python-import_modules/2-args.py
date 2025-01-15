e03c26ffebb81ce892f99eb4059311e8bc0b83d2
seb25000 <seb25000@gmail.com>
2025-01-15 20:10:57 +0100

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv
+    num_args = len(argv) - 1  # Exclude the script name itself
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, argv[i]))
