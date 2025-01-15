9f210646b242195a6b277da2f0128b5d3cb510b3
seb25000 <seb25000@gmail.com>
2025-01-15 14:11:25 +0100

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,5 @@
 for i in range(100):
-    print("{:02d}, ".format(i), end="" if i < 99 else "\n")
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
