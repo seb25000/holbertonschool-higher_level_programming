2d70bcf5b9b181f2216e9c559da85e49b038e59b
seb25000 <seb25000@gmail.com>
2025-01-15 15:36:53 +0100

    11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
+
 def pow(a, b):
     """Computes a to the power of b.
 
     Args:
-      a: The base number.
-      b: The exponent.
+        a: The base number.
+        b: The exponent.
 
     Returns:
         The value of a^b.
     """
     if b == 0:
         return 1
-    if b > 0:
-      result = 1
-      for _ in range(b):
-        result *= a
-      return result
-    else:
-      result = 1
-      for _ in range(abs(b)):
-        result /= a
-      return result
+    if b < 0:
+       return 1 / power_positive(a, -b)
+    return power_positive(a,b)
+def power_positive(a,b):
+    result = 1
+    for _ in range(b):
+      result *= a
+    return result
+
+if __name__ == "__main__":
+    pow_func = pow
+    print(pow_func(2, 2))
+    print(pow_func(98, 2))
+    print(pow_func(98, 0))
+    print(pow_func(100, -2))
+    print(pow_func(-4, 5))
