93d85ce9d43042836c8be76f7ff3c21882416570
seb25000 <seb25000@gmail.com>
2025-01-15 15:32:17 +0100

    10-add.py

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+
 def add(a, b):
-    """Adds two integers and returns the result."""
-    return a + b
+  """Adds two integers and returns the result.
+
+  Args:
+    a: The first integer.
+    b: The second integer.
+
+  Returns:
+    The sum of a and b.
+  """
+  return a + b
+
+if __name__ == "__main__":
+    add = __import__('10-add').add
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
