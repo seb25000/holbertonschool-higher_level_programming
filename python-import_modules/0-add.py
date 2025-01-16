76abbf6318def6ffe474ebfe0d0d00f49997ae7c
seb25000 <seb25000@gmail.com>
2025-01-16 12:47:56 +0100

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    """Adds two integers.
-
-    Args:
-        a: The first integer.
-        b: The second integer.
-
-    Returns:
-        The sum of a and b.
-    """
-    return a + b
-
 if __name__ == "__main__":
     from add_0 import add
 
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
 def add(a, b):
     """
     My add function
