d4f43769c34d740003db1c7a2167252433424f17
seb25000 <seb25000@gmail.com>
2025-01-16 12:37:27 +0100

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 
+def add(a, b):
+    """Adds two integers.
+
+    Args:
+        a: The first integer.
+        b: The second integer.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
+
 if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
