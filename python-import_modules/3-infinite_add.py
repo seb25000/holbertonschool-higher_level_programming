6be7ddb77f5958bd46af600e7e072ff5ce4c17da
seb25000 <seb25000@gmail.com>
2025-01-17 21:27:31 +0100

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-import sys
+
+from sys import argv
+
 
 def main():
     # Récupérer les arguments passés au script
     arguments = sys.argv[1:]
-    
+
     # Initialiser la somme à 0
     total = 0
-    
+
     # Ajouter chaque argument (converti en entier) à la somme
     for arg in arguments:
         total += int(arg)
-    
+
     # Afficher le total
     print(total)
+
 
 if __name__ == "__main__":
     main()
