9ba1e91be81df22819d0200e923135ee5c3083f2
seb25000 <seb25000@gmail.com>
2025-01-18 23:26:53 +0100

    5-variable_load.py

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-import importlib.util
-import os
-
 
 if __name__ == "__main__":
-    file_path = "variable_load_5.py"
+    import importlib.util
+    import os
+file_path = "variable_load_5.py"
 
-    spec = importlib.util.spec_from_file_location("variable_load_5", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+spec = importlib.util.spec_from_file_location("variable_load_5", file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
 
-    print(module.a)
+print(module.a)
