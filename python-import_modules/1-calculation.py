a19612d7ef64da302b2fffd2b1ea56b6cd6181a2
seb25000 <seb25000@gmail.com>
2025-01-19 17:03:25 +0100

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 
-import calculator_1
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
 
-# Définition des variables
-a = 10
-b = 5
-
-# Effectuer les calculs et stocker les résultats
-addition_result = calculator_1.add(a, b)
-subtraction_result = calculator_1.sub(a, b)
-multiplication_result = calculator_1.mul(a, b)
-division_result = calculator_1.div(a, b)
-
-# Affichage des résultats avec seulement 4 appels à print
-print(f"{a} + {b} = {addition_result}")
-print(f"{a} - {b} = {subtraction_result}")
-print(f"{a} * {b} = {multiplication_result}")
-print(f"{a} / {b} = {division_result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
