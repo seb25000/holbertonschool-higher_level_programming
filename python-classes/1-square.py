0715b5d8647fa0d8e41df505b6a99fa1cc9a72a6
seb25000 <seb25000@gmail.com>
2025-01-27 11:00:00 +0100

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates class Square with private instance attribute size"""
+
+
+class Square:
+    """defines class and instantiates private instance attribute size."""
+
+    def __init__(self, size):
+        self.__size = size
