bb9b0aebd5da4579c0347322b847c269a755397c
seb25000 <seb25000@gmail.com>
2025-02-10 19:37:42 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 
 from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     arglist = load_from_json_file("add_item.json")
