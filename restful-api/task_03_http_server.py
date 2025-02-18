651c0a0adc1f79c9e1ac1671c30fcdfa4985fc25
seb25000 <seb25000@gmail.com>
2025-02-18 17:52:21 +0100

    task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,52 +1,43 @@
-#!/usr/bin/python3
-"""Python web server"""
 import http.server
 import socketserver
 import json
-
+from urllib.parse import urlparse
 
 PORT = 8000
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/":
+        parsed_url = urlparse(self.path)
+        path = parsed_url.path
+
+        if path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-
-        elif self.path == "/data":
+        elif path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            response_data = {
-                "name": "John",
-                "age": 30,
-                "city": "New York"
-            }
-            self.wfile.write(json.dumps(response_data).encode('utf-8'))
-        elif self.path == "/status":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.wfile.write(json_data.encode("utf-8"))
+        elif path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "status": "OK"
-            }"""
             self.wfile.write(b"OK")
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "error": "Endpoint not found"
-            }"""
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 Handler = MyHandler
 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
+    print("serving at port", PORT)
     httpd.serve_forever()
