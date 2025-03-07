f2d081188dc6878304185d03a3284b845e963346
seb25000 <seb25000@gmail.com>
2025-03-07 21:43:20 +0100

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
 """0x0F. Python - Object-relational mapping - task 0. Get all states"""
 
-import sys
-import pymysql
-
 if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
     if len(sys.argv) != 4:
-        sys.exit('Use: 0-select_states.py <mysql username> <mysql password> <database name>')
+        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
+                 ' <database name>')
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        conn = pymysql.connect(host='localhost', port=3306, user=username,
-                               password=password, db=database, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM states ORDER BY id ASC")
-            results = cur.fetchall()
-
-        for row in results:
-            print(row)
-
-    except pymysql.MySQLError as e:
-        print(f"Error connecting to MySQL: {e}")
-
-    finally:
-        if conn:
-            conn.close()
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
